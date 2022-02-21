026fe53e72dfbf4e6b953c46adca645bb45aef16
Ashutosh Kumar <ashu3324tosh@gmail.com>
2022-02-21 21:00:37 +0530

    third commit

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x32DA2C1bec1368f860d1c7806E9fd4c586705327";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://cryptodevs-nft.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
